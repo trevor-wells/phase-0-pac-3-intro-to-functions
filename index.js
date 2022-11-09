c2331d22a4048631c9997632476b3dedee7eb60f
trevorwells <trevorwells@g.ucla.edu>
2022-11-08 19:42:09 -0700

    Final Commit

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+}
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+}
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+function doSomething(thing) {
+    console.log(thing);
+}
+
+function add(x, y) {
+    return x + y;
+}
+  
+sayHello();
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+doSomething("Something");
+sayHelloTo("Trevor Wells");
+say("Frankly my dear", "I don't give a damn");
+console.log(add(9, 10));
